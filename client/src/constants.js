3c6818210a339ca15552d34b58eb8cfea100bdc5
Grant Cloyd <dgcloyd@gmail.com>
2021-08-06 13:27:27 -0500

    trying to set api call

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
-export const API_ROOT = "http://localhost:3000"
-export const API_WS_ROOT = "ws://localhost:3000/cable"
+export const API_ROOT = "http://ec2-44-196-250-191.compute-1.amazonaws.com"
+export const API_WS_ROOT = "ws://ec2-44-196-250-191.compute-1.amazonaws.com/cable"
 export const HEADERS = {
    "Content-Type": "application/json",
    Accept: "application/json",
