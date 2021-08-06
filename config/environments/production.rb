e0a29d63bf0e85c25ccecc35b5caac46d4765ce1
Grant Cloyd <dgcloyd@gmail.com>
2021-08-06 15:47:08 -0500

    trying redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.web_socket_server_url = "wss://action-cable-example.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['http://infinite-cove-97966.herokuapp.com/find', 'https://infinite-cove-97966.herokuapp.com/find']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
